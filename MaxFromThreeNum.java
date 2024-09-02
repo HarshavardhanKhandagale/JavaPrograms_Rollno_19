974a10c100d91fac0d02465998461fc2919ea294
HarshavardhanKhandagale <harshvardhankhandagale@gmail.com>
2024-09-02 13:52:59 +0530

    Delete MaxFromThreeNum.java
    yes

--- MaxFromThreeNum.java
+++
@@ -1,8 +0,0 @@
-public class MaxFromThreeNum {
-
-    public static voidmane(string[]args)
-    {
-        int No1 = 0, No2 = 0, No3 = 0;
-        Scanner sc = new Scanner(System.in);
-         
-    }}
